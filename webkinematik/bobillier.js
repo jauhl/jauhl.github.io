a4f03ae5b7f25aa8f3a9ae3e7b9747b4e7a5e965
Jan Uhlig <jan.uhlig@web.de>
2016-12-04 21:28:50 +0100

    release 1.0

--- webkinematik/bobillier.js
+++ webkinematik/bobillier.js
@@ -1,6 +1,9 @@
 function position() {
     g.del().style({foc:"white"})
 
+    if (panel.getValue("Wendekreis anzeigen"))
+        g.cir(mec.center.x, mec.center.y, mec.radius, {ls:"orange"})
+
      // Schritt 1
      if(mec.step >= 1) {
         g.use("pol",mec.P).label("P","n")
@@ -18,6 +21,38 @@ function position() {
             g.lin(mec.B0.x, mec.B0.y, mec.A0.x, mec.A0.y, {ld:"@dash", ls:"rgba(255, 255, 255, 0.7)"}) 
         }
      }
+
+     // Schritt 3
+     if(mec.step >= 3) {
+        g.lin(mec.I.x, mec.I.y, mec.P.x, mec.P.y, {ls:"rgba(255, 255, 255, 0.7)"}).mark("tick",0.49).mark("tick",0.51)    
+     }
+
+     // Schritt 4
+     if(mec.step >= 4) {
+        g.use("nod",mec.K).label("K","se")
+         .lin(mec.P.x, mec.P.y, mec.K.x, mec.K.y, {ls:"rgba(255, 255, 255, 0.7)"}).mark("tick",0.49).mark("tick",0.51) 
+     }
+
+     // Schritt 6
+     if(mec.step >= 6) {
+        g.use("pol",mec.W).label("W","se")
+         .lin(mec.Aw.x, mec.Aw.y, mec.W.x, mec.W.y, {ld:"@dot", ls:"rgba(255, 255, 255, 0.8)"})
+         .lin(mec.Bw.x, mec.Bw.y, mec.W.x, mec.W.y, {ld:"@dot", ls:"rgba(255, 255, 255, 0.8)"})
+         .beg({x: mec.Aw.x, y: mec.Aw.y, w:pi + Math.atan2(mec.e_A0A.y, mec.e_A0A.x)})
+            .rec(0, 0, -8, 8, {lw:2, ls:"rgba(126, 126, 118, 0.9)", fs:"rgba(255, 255, 255, 0.6)"})
+         .end()
+         .beg({x: mec.Bw.x, y: mec.Bw.y, w:Math.atan2(mec.e_B0B.y, mec.e_B0B.x)})
+            .rec(0, 0, 8, 8, {lw:2, ls:"rgba(126, 126, 118, 0.9)", fs:"rgba(255, 255, 255, 0.6)"})
+         .end()
+     }
+
+     // Schritt 5
+     if(mec.step >= 5) {
+        // var pos = 0.5*norm(v2.len(v2.dif(mec.Bw, mec.K)), 0, v2.len(v2.dif(mec.Aw, mec.K))); // halbe normalisierte Position von Bw auf KAw
+        g.use("nod",mec.Aw).label("Aw","se")
+         .use("nod",mec.Bw).label("Bw","se")
+         .lin(mec.K.x, mec.K.y, mec.Aw.x, mec.Aw.y, {ls:"rgba(255, 255, 255, 0.8)"}).mark("tick",0.49).mark("tick",0.51) 
+     }  
 
      g.bar2(mec.A, mec.B)
       .use("nod",mec.A).label("A","nw")
@@ -38,6 +73,10 @@ function updateOutput() {
     panel2.setValue("aktueller Schritt:", steps[mec.step]);
 }
 
+function norm(value, min, max) {  
+    return (value - min)/(max - min);
+}
+
 var cnv = document.getElementById('c'),
     ctx = cnv.getContext('2d'),
     width = cnv.width = window.innerWidth,
@@ -48,8 +87,8 @@ var cnv = document.getElementById('c'),
     mec = {
         phi:60*pi/180,
         a:45,
-        b:150,
-        c:100,
+        b:120,
+        c:130,
         d:200,
         e:0,
 
@@ -59,20 +98,158 @@ var cnv = document.getElementById('c'),
         //#            Loesung
         //######################################################################
 
+        // Schritt 1
         get r() {
-            return (this.A0.y - this.B0.y + (this.B0.x - this.A0.x)*(this.ephi.y/this.ephi.x))/(this.epsi.y - (this.ephi.y*this.epsi.x)/this.ephi.x);
+            var frac = this.ephi.y/this.ephi.x;
+            return (this.A0.y - this.B0.y + (this.B0.x - this.A0.x)*(frac))/(this.epsi.y - (frac*this.epsi.x));
         },
 
         get P() {
             return (v2.isum(v2.scl(this.epsi,this.r), this.B0));
         },
+        
+        // get P() {
+        //     return  {x: this.B0.x + this.r*this.epsi.x, y:this.B0.y + this.r*this.epsi.y};
+        // },
 
+        // Schritt 2
         get k() {
-            return (this.A.y - this.A0.y + (this.A0.x - this.A.x)*(this.etheta.y/this.etheta.x))/(this.ealpha.y - (this.etheta.y*this.ealpha.x)/this.etheta.x);
+            var frac = this.etheta.y/this.etheta.x;
+            return (this.A.y - this.A0.y + (this.A0.x - this.A.x)*(frac))/(this.ealpha.y - (frac*this.ealpha.x));
         },
 
         get I() {
             return (v2.isum(v2.scl(this.ealpha,this.k), this.A0));
+        },
+
+        // get I() {
+        //     return {x: this.A0.x + this.k*this.ealpha.x, y:this.A0.y + this.k*this.ealpha.y};
+        // },
+        
+        // Schritt 4
+        get e_A0B0() {
+            return (v2.iunit(v2.dif(this.B0, this.A0)));
+        },
+
+        get e_AB() {
+            return (v2.iunit(v2.dif(this.B, this.A)));
+        },
+
+        get s() {
+            var frac = this.e_A0B0.y/this.e_A0B0.x;
+            return (this.P.y - this.A.y + (this.A.x - this.P.x)*frac)/(this.e_AB.y - (frac*this.e_AB.x));
+        },
+
+        get K() {
+            return (v2.isum(v2.scl(this.e_AB,this.s), this.A));
+        },
+
+        // get K() {
+        //     return {x: this.A.x + this.s*this.e_AB.x, y:this.A.y + this.s*this.e_AB.y};
+        // },
+
+        // Schritt 5
+        get e_PI() {
+            return (v2.iunit(v2.dif(this.I, this.P)));
+        },
+
+        get e_A0A() {
+            return (v2.unit(this.A));
+        },
+
+        get e_B0B() {
+            return (v2.iunit(v2.dif(this.B, this.B0)));
+        },
+
+        // numerisches Problem bei phi=0° und phi=360° (innere Steglage). frac sowie this.A0.y sind dort geometriebedingt 0. Als Folge wird durch 0 geteilt (verfahrensbedingt)
+        get t() {
+            var frac = this.e_PI.y/this.e_PI.x;
+            return (this.K.y - this.A0.y + (this.A0.x - this.K.x)*frac)/(this.e_A0A.y - (frac*this.e_A0A.x));
+        },
+
+        get Aw() {
+            return (v2.isum(v2.scl(this.e_A0A,this.t), this.A0));
+        },
+
+        // get Aw() {
+        //     return {x: this.A0.x + this.t*this.e_A0A.x, y:this.A0.y + this.t*this.e_A0A.y};
+        // },
+
+        get u() {
+            var frac = this.e_PI.y/this.e_PI.x;
+            return (this.K.y - this.B0.y + (this.B0.x - this.K.x)*frac)/(this.e_B0B.y - (frac*this.e_B0B.x));
+        },
+
+        get Bw() {
+            return (v2.isum(v2.scl(this.e_B0B,this.u), this.B0));
+        },
+
+        // get Bw() {
+        //     return {x: this.B0.x + this.u*this.e_B0B.x, y:this.B0.y + this.u*this.e_B0B.y};
+        // },
+
+        // Schritt 6
+        get v() {
+            var frac = - this.e_A0A.x/this.e_A0A.y;
+            return (this.Aw.y - this.Bw.y + (this.Bw.x - this.Aw.x)*frac)/(this.e_B0B.x + (frac*this.e_B0B.y));
+        },
+
+        get W() {
+            return (v2.isum(v2.iscl(v2.tilde(this.e_B0B),this.v), this.Bw));
+        },
+
+        // get W() {
+        //     return {x: this.Bw.x + this.v*v2.tilde(this.e_B0B).x, y:this.Bw.y + this.v*v2.tilde(this.e_B0B).y};
+        // },
+
+        // Wendekreis
+        get G1() {
+            return ((v2.dif(this.P, this.Aw))); //+++++++++
+        },
+
+        get G2() {
+            return ((v2.dif(this.P, this.Bw))); //+++++++++
+        },
+
+        get e_G1() {
+            return (v2.unit(this.G1));
+        },
+
+        get e_G2() {
+            return (v2.unit(this.G2));
+        },
+
+        get H1() {
+            return (v2.isum(v2.scl(this.e_G1, 0.5*(v2.len(this.G1))), this.Aw));
+        },
+
+        // get H1() {
+        //     return {x: this.Aw.x + 0.5*(v2.len(this.G1))*this.e_G1.x, y:this.Aw.y + 0.5*(v2.len(this.G1))*this.e_G1.y};
+        // },
+
+        get H2() {
+            return (v2.isum(v2.scl(this.e_G2, 0.5*(v2.len(this.G2))), this.Bw));
+        },
+
+        // get H2() {
+        //     return {x: this.Bw.x + 0.5*(v2.len(this.G2))*this.e_G2.x, y:this.Bw.y + 0.5*(v2.len(this.G2))*this.e_G2.y};
+        // },
+
+        get w() {
+            var frac = - this.e_G1.x/this.e_G1.y;
+            return (this.H1.y - this.H2.y + (this.H2.x - this.H1.x)*frac)/(this.e_G2.x + (frac*this.e_G2.y));
+        },
+
+        get center() {
+            return (v2.isum(v2.scl(v2.tilde(this.e_G2),this.w), this.H2));
+        },
+
+        // get center() {
+        //     return {x: this.H2.x + this.w*v2.tilde(this.e_G2).x, y:this.H2.y + this.w*v2.tilde(this.e_G2).y};
+        // },
+
+        get radius() {
+            return (v2.len(v2.dif(this.center, this.Aw)));
         },
 
         //######################################################################
@@ -145,12 +322,8 @@ var cnv = document.getElementById('c'),
     g = g2(),
     world = g2().clr().cartesian().style({foc:"white"})
                 .rec(-width,-height,2*width,2*height,{ls:"rgba(52, 76, 107, 1)",fs:"rgba(52, 76, 107, 1)"})  // blueprint blue rgba(52, 76, 107, 1)
-                // .beg({lw:5})
-                    .grid("rgba(255, 255, 255, 0.1)",100)
-                // .end()
-                // .beg({lw:0.15})
-                    .grid("rgba(255, 255, 255, 0.1)",20)
-                // .end()
+                .grid("rgba(255, 255, 255, 0.1)",100)
+                .grid("rgba(255, 255, 255, 0.1)",20)
                 .pan((width - (mec.a + mec.d))/2, (height - mec.c)/2) 
                 .use(g)
                 .use("gnd").label("A0","se"),
@@ -160,8 +333,11 @@ var cnv = document.getElementById('c'),
     steps = {
         0: "Bekannt ist die Lage zweier Punkte (A, B) eines Gliedes, sowie deren Krümmungsmittelpunkte A\u2080 und B\u2080",
         1: "Ermittlung des Momentanpols P im Schnittpunkt der Geraden AA\u2080 und BB\u2080.",
-        2: "Bestimmung des Relativpols I zwischen Kurbel und Schwinge im Schnittpunkt der Geraden AB und A\u2080B\u2080"
-        // weitere Schritte
+        2: "Bestimmung des Relativpols I zwischen Kurbel und Schwinge im Schnittpunkt der Geraden AB und A\u2080B\u2080",
+        3: "Festlegung der Kollineationsachse als Gerade durch P und I.",
+        4: "Die Parallele zu A\u2080B\u2080 durch P liefert im Schnittpunkt mit AB den Hilfspunkt K.",
+        5: "Die Parallele zu PI durch diesen Hilfspunkt K schneidet die Gerade AA\u2080 in Aw und die Gerade BB\u2080 in Bw. Aw und Bw liegen beide auf dem Wendekreis.",
+        6: "Die Senkrechten in Aw zu AA\u2080 und in Bw zu BB\u2080 schneiden sich im Wendepol W."
     },
 
     panel = QuickSettings.create(10, 10, "Steuerung")
@@ -169,16 +345,17 @@ var cnv = document.getElementById('c'),
              .addProgressBar("Fortschritt", 6, mec.step, "numbers")
              .addButton("Schritt weiter ▶", function(value) { if(mec.step<6) {mec.step+=1; dirty = true; panel.setValue("Fortschritt", mec.step);} updateOutput();}) // ▶
              .addButton("Schritt zurück ◀", function(value) { if(mec.step>0) {mec.step-=1; dirty = true; panel.setValue("Fortschritt", mec.step);} updateOutput();}) // ◀
+             .addBoolean("Wendekreis anzeigen", false, function() {dirty = true;})
              .addRange("φ", 0, 360, 60, 1, function(value) { mec.phi = value / 180 * pi; dirty = true;})
              .addNumber("a", 0, 300, 45, 1, function(value) { mec.a = value; dirty = true;})
-             .addNumber("b", 0, 300, 150, 1, function(value) { mec.b = value; dirty = true;})
-             .addNumber("c", 0, 300, 100, 1, function(value) { mec.c = value; dirty = true;})
+             .addNumber("b", 0, 300, 120, 1, function(value) { mec.b = value; dirty = true;})  // "b", 0, 300, 150, 1
+             .addNumber("c", 0, 300, 130, 1, function(value) { mec.c = value; dirty = true;})  // "c", 0, 300, 100, 1
              .addNumber("d (rein horizontal)", 0, 300, 200, 1, function(value) { mec.d = value; dirty = true;})
              .addNumber("e (rein vertikal)", -100, 100, 0, 1, function(value) { mec.e = value; dirty = true;}),
 
     panel2 = QuickSettings.create(width - 310, 10, "Output")
              .addTextArea("aktueller Schritt:")
-             .setWidth(300)            
+             .setWidth(300)
 ;
 
 /*
@@ -188,7 +365,3 @@ var cnv = document.getElementById('c'),
 // Animation starten
 updateOutput();
 render();
-
-window.onload = function() {
-    var test = 1;
-}
